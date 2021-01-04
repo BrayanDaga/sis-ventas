40c7425fe072247a144174e7020c774daf675b37
Brayan Vilchez Daga <brayandaga5@gmail.com>
2021-01-04 00:03:39 -0500

    dashboardv-3

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Sale;
+use App\Purchase;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -23,6 +26,26 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $purchaseCounts = Purchase::select(DB::raw('MONTH(created_at) as month'),
+        DB::raw('COUNT(1) as count'))->groupby('month')->get()->toArray();
+
+        $saleCounts = Sale::select(DB::raw('MONTH(created_at) as month'),
+        DB::raw('COUNT(1) as count'))->groupby('month')->get()->toArray();
+
+
+        // $purchases = array_fill(0,12,0); //index , qty ,value
+        // $sales = array_fill(0,12,0); //index , qty ,value
+        $purchases = [];
+        $sales = [];
+        foreach ($purchaseCounts as $monthlyCount) {
+            $index = $monthlyCount['month']-1;
+            $purchases[$index] = $monthlyCount['count'];
+        }
+        foreach ($saleCounts as $monthlyCount) {
+            $index = $monthlyCount['month']-1;
+            $sales[$index] = $monthlyCount['count'];
+        }
+        //  dd($purchases);
+        return view('home',compact('sales','purchases'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,38 +8,13 @@
 @stop
 
 @section('content')
-    <div class="row">
 
-      <div class="col-md-6">
-        <!-- AREA CHART -->
-        <div class="card card-primary">
-          <div class="card-header with-border">
-            <h3 class="card-title">Area Chart</h3>
-          </div>
-          <div class="card-body">
-            <div class="chart">
-              <canvas id="areaChart" style="height:250px"></canvas>
-            </div>
-          </div>
-          <!-- /.card-body -->
-        </div>
-        <!-- /.card -->
 
-        <!-- DONUT CHART -->
-        <div class="card card-danger">
-          <div class="card-header with-border">
-            <h3 class="card-title">Donut Chart</h3>
-          </div>
-          <div class="card-body">
-            <canvas id="pieChart" style="height:250px"></canvas>
-          </div>
-          <!-- /.card-body -->
-        </div>
-        <!-- /.card -->
+@include('includes.cards-status')
 
-      </div>
+  <div class="row">
       <!-- /.col (LEFT) -->
-      <div class="col-md-6">
+      <div class="col-md-9">
         <!-- LINE CHART -->
         <div class="card card-info">
           <div class="card-header with-border">
@@ -55,23 +30,19 @@
         </div>
         <!-- /.card -->
 
-        <!-- BAR CHART -->
-        <div class="card card-success">
-          <div class="card-header with-border">
-            <h3 class="card-title">Bar Chart</h3>
-
-          </div>
-          <div class="card-body">
-            <div class="chart">
-              <canvas id="barChart" style="height:230px"></canvas>
-            </div>
-          </div>
-          <!-- /.card-body -->
-        </div>
-        <!-- /.card -->
+    
 
       </div>
       <!-- /.col (RIGHT) -->
+  
+      <div class="col-md-3">
+          <div class="card">
+              <div class="card-body">
+                  <h5 class="card-title">Title</h5>
+                  <p class="card-text">Content</p>
+              </div>
+          </div>
+      </div>
     </div>
     <!-- /.row -->
 
@@ -81,40 +52,13 @@
 <script>
 
 
-var areaCanva = document.getElementById('areaChart').getContext('2d');
-var areaChart = new Chart(areaCanva, {
-    // The type of chart we want to create
-    type: 'line',
-
-    // The data for our dataset
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [
-        {
-          label               : 'Electronics',
-
-          data                : [65, 59, 80, 81, 56, 55, 40]
-        },
-        {
-            label               : 'Digital Goods',
-            backgroundColor :     'rgba(54, 162, 235, 0.2)',
-
-          data                : [28, 48, 40, 19, 86, 27, 90]
-        }
-      ]
-    },
-
-    // Configuration options go here
-    options: {
-    }
-});
 
 
 var lineCanva          = $('#lineChart').get(0).getContext('2d');
 
 var dataFirst = {
   label: "Car A - Speed (mph)",
-  data: [0, 59, 75, 20, 20, 55, 40],
+  data: @json($purchases),
  lineTension: 0,
     fill: false,
     borderColor: 'orange',
@@ -134,7 +78,7 @@ var dataFirst = {
 
 var dataSecond = {
   label: "Car B - Speed (mph)",
-  data: [20, 15, 60, 60, 65, 30, 70],
+  data: @json($sales),
   lineTension: 0,
     fill: false,
     borderColor: 'blue',
@@ -163,46 +107,6 @@ type: 'line',
   data: speedData
 })
 
-
-var pieCanvas = $('#pieChart').get(0).getContext('2d')
-var myPieChart = new Chart(pieCanvas, {
-    type: 'pie',
-   data :  {
-    datasets: [{
-        backgroundColor :[
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-        ],
-        data: [
-            10, 20, 30
-        ]
-    }],
-
-    // These labels appear in the legend and in the tooltips when hovering different arcs
-    labels: [
-        'Red',
-        'Yellow',
-        'Blue'
-    ],
-
-},
-    options: {}
-});
-
-var barCanvas = $('#barChart').get(0).getContext('2d');
-var densityData = {
-  label: 'Density of Planets (kg/m3)',
-  data: [5427, 5243, 5514, 3933, 1326, 687, 1271, 1638]
-};
-var myBarChart = new Chart(barCanvas, {
-    type: 'bar',
-  data: {
-    labels: ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"],
-    datasets: [densityData]
-  }
-
-});
 
 </script>
 @stop
